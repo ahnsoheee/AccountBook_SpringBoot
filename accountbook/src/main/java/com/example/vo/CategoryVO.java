9510fc3d3aa8b98c5046752fc8f659eb3775d42b
ahnsoheee <soheeeahn@gmail.com>
2021-06-24 17:46:42 +0900

    [be] feat: categoryVO 수정 - type 추가

--- accountbook/src/main/java/com/example/vo/CategoryVO.java
+++ accountbook/src/main/java/com/example/vo/CategoryVO.java
@@ -3,6 +3,7 @@ package com.example.vo;
 public class CategoryVO {
     int id;
     int type_id;
+    String type;
     String user_id;
     String name;
 
@@ -14,19 +15,27 @@ public class CategoryVO {
         this.id = id;
     }
 
-    public int getTypeId() {
+    public int getType_id() {
         return type_id;
     }
 
-    public void setTypeId(int type_id) {
+    public void setType_id(int type_id) {
         this.type_id = type_id;
     }
 
-    public String getUserId() {
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUser_id() {
         return user_id;
     }
 
-    public void setUserId(String user_id) {
+    public void setUser_id(String user_id) {
         this.user_id = user_id;
     }
 
